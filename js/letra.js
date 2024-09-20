72507d4036253114c90b1fa6f03a623fb9f2912c
JoseAlbertoLuyoHuari <jose.luyo.huari@vallegrande.edu.pe>
2024-09-20 10:31:28 -0500

    implementation of main

--- js/letra.js
+++ js/letra.js
@@ -1,1 +1,6 @@
-// Aqui va el codigo de letra.js
+var typed = new Typed(".auto-type", {
+    strings: ["Valle Grande", "Programadores", "Estudiantes"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    looped: true
+  })
