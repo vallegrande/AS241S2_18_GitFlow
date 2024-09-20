f673096549a6030dde430d2d3cff89c9e99492cf
SnayderVIcente <snayder.vicente@vallegrande.edu.pe>
2024-09-20 10:14:06 -0500

    implementation of header

--- js/header.js
+++ js/header.js
@@ -1,1 +1,4 @@
-// Aqui va el codigo del header.js
+const header = document.querySelector("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", this.window.scrollY > 60)
+});
